9d8cc72e16575b772a4110f87967702a2f192bf4
ciolak74 <42117544+ciolak74@users.noreply.github.com>
2023-08-04 13:00:31 +0200

    10

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/greeting', (request, response) => {
   const name = request.query ? request.query.name : undefined;
-  response.send({ content: `Hello9, ${name || 'World!'}` });
+  response.send({ content: `Hello10, ${name || 'World!'}` });
 });
 
 module.exports = app;
